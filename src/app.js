f357de6c959189f5ee43f4e602ff3e11d9c8673e
Matt Zeunert <matt@mostlystatic.com>
2016-06-03 08:22:03 +0100

    Read in code from Github page.

--- src/app.js
+++ src/app.js
@@ -1,29 +1,37 @@
-var tern = require("tern")
-var estraverse = require("estraverse")
+var $ = require("jquery")
+var readCodeBlock = require("./read-code-block")
 
-var srv = new tern.Server({});
-var identifiers = []
-srv.on("postParse", function(ast){
-    estraverse.traverse(ast, {
-        enter: function (node, parent) {
-            if (node.type === "Identifier" && parent.type==="VariableDeclarator") {
-                identifiers.push(node.start)
-            }
-        }
-    });
-})
+var codeBlock = readCodeBlock($(".blob-wrapper").first(0))
 
-srv.addFile("test.js", "var a = 5;   \na=10;\n a=44;")
-                     // 01234567890123456789
-srv.flush(function(){});
+console.log(codeBlock)
 
-var doc = {
-    query: {
-        type: "refs",
-        file: "test.js",
-        end: identifiers[0]
-    }
-}
-srv.request(doc, function(){
-    console.log(arguments[1].refs)
-})
+//
+// var tern = require("tern")
+// var estraverse = require("estraverse")
+//
+// var srv = new tern.Server({});
+// var identifiers = []
+// srv.on("postParse", function(ast){
+//     estraverse.traverse(ast, {
+//         enter: function (node, parent) {
+//             if (node.type === "Identifier" && parent.type==="VariableDeclarator") {
+//                 identifiers.push(node.start)
+//             }
+//         }
+//     });
+// })
+//
+// srv.addFile("test.js", "var a = 5;   \na=10;\n a=44;")
+//                      // 01234567890123456789
+// srv.flush(function(){});
+//
+// var doc = {
+//     query: {
+//         type: "refs",
+//         file: "test.js",
+//         end: identifiers[0]
+//     }
+// }
+// srv.request(doc, function(){
+//     console.log(arguments[1].refs)
+// })
---
+++ src/read-code-block.js
@@ -0,0 +1,61 @@
+var $ = require("jquery")
+
+function getCodeParts(containerEl){
+    var lineElements = $(containerEl).find(".js-file-line");
+
+    var codeParts = []
+    lineElements.each(function(i, lineElement){
+        lineElement = $(lineElement)
+        processContent(lineElement)
+
+        codeParts.push({
+            el: null,
+            lineElement: lineElement,
+            content: "\n"
+        })
+
+        function processContent(el){
+            el.contents().each(function(i, contentEl){
+                contentEl = $(contentEl)
+                var contents = contentEl.contents()
+                if (contents.length > 1) {
+                    processContent(contentEl)
+                } else {
+                    codeParts.push({
+                        el: contentEl[0],
+                        lineElement: lineElement,
+                        content: contentEl.text()
+                    })
+                }
+            })
+        }
+    })
+
+    var pos = 0;
+    codeParts.forEach(function(codePart){
+        codePart.start = pos;
+        pos += codePart.content.length;
+        codePart.end = pos;
+        $(codePart.el).css("border", "1px solid red")
+    })
+
+    return codeParts
+}
+
+function getCodeFromCodeParts(codeParts){
+    var code = "";
+    codeParts.forEach(function(codePart){
+        code += codePart.content
+    })
+    return code;
+}
+
+function readCodeBlock(el){
+    var codeParts = getCodeParts(el);
+    return {
+        codeParts: codeParts,
+        code: getCodeFromCodeParts(codeParts)
+    }
+}
+
+module.exports = readCodeBlock
